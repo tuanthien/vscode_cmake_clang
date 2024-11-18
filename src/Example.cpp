a8c733f507d46d52918080c848ee70d0b225f361
Tsuneka <tsuneka.kou@gmail.com>
2024-11-18 23:04:52 +0700

    dumb everything down for cakez77

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,9 +1,11 @@
 #include "raylib.h"
 
-int main(void) {
+int main(void)
+{
   InitWindow(800, 450, "raylib [core] example - basic window");
 
-  while (!WindowShouldClose()) {
+  while (!WindowShouldClose())
+  {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Congrats! You created your first window!", 190, 200, 20,
